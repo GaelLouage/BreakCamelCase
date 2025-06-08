108c036ed82e44cf0b980d69259fb6fd5a4b0dfe
Gaël Louage <gaellouage@hotmail.com>
2025-06-08 11:03:49 +0200

    BreakCamelCase

---
+++ BreakCamelCaseExercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(BreakCamelCaseLinq("camelCasing"));
+    }
+
+    public static string BreakCamelCase(string str)
+    {
+        if (string.IsNullOrEmpty(str)) return string.Empty;
+        var sb = new StringBuilder();
+        foreach (var c in str)
+        {
+            if (char.IsUpper(c))
+            {
+                sb.Append($" {c}");
+            }
+            else
+            {
+                sb.Append(c);
+            }
+        }
+        return sb.ToString();
+    }
+
+    public static string BreakCamelCaseLinq(string str)
+    {
+        if (string.IsNullOrEmpty(str)) return string.Empty;
+
+        return string.Join("", str.Select(c =>
+        {
+            if (char.IsUpper(c))
+            {
+                return $" {c}";
+            }
+            return $"{c}";
+        }));
+    }
+}
